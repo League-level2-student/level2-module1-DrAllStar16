6f1b82faa75370f5d1493fac0be3e01f3633ed4c
DrAllStar16 <league@WTS14.attlocal.net>
2018-10-01 16:56:34 -0700

    ArrayOfInt

--- src/intro_to_array_lists/ArrayOfINT.java
+++ src/intro_to_array_lists/ArrayOfINT.java
@@ -1,5 +1,23 @@
 package intro_to_array_lists;
 
-public class ArrayOfINT {
+import java.util.Random;
 
+public class ArrayOfINT {
+	public static void main(String[] args) {
+
+		Random r = new Random();
+		int s = 0;
+		int i;
+		int array[] = new int[50];
+		for (int j = 0; j < array.length; j++) {
+			i = r.nextInt(100);
+			array[j] = i;
+		}
+		for (int m = 0; m < array.length; m++) {
+			s = array[m] + s;
+		}
+		System.out.println("Your average is " + s / 50);
+		System.out.println();
+		System.out.println("Your total of all of the numbers is " + s);
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -4,40 +4,40 @@ import java.util.ArrayList;
 
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
 		ArrayList<String> bbt = new ArrayList<String>();
-		//2. Add five Strings to your list
-	bbt.add("Cubs");
-	bbt.add("Padres");
-	bbt.add("Angles");
-	bbt.add("Pirates");
-	bbt.add("Dodgers");
-		//3. Print all the Strings using a standard for-loop
+		// 2. Add five Strings to your list
+		bbt.add("Cubs");
+		bbt.add("Padres");
+		bbt.add("Angles");
+		bbt.add("Pirates");
+		bbt.add("Dodgers");
+		// 3. Print all the Strings using a standard for-loop
 		for (int i = 0; i < bbt.size(); i++) {
 			System.out.println(bbt.get(i));
 		}
-		//4. Print all the Strings using a for-each loop
+		// 4. Print all the Strings using a for-each loop
 		System.out.println();
-		for(String s: bbt) {
+		for (String s : bbt) {
 			System.out.println(s);
 		}
-		//5. Print only the even numbered elements in the list.
+		// 5. Print only the even numbered elements in the list.
 		System.out.println();
 		for (int i = 0; i < bbt.size(); i++) {
-			if (i%2==0) {
+			if (i % 2 == 0) {
 				System.out.println(bbt.get(i));
 			}
 		}
-		//6. Print all the Strings in reverse order.
+		// 6. Print all the Strings in reverse order.
 		System.out.println();
-			
-		
+
 		for (int i = bbt.size(); i > 0; i--) {
-			System.out.println(bbt.get(i-1));
+			System.out.println(bbt.get(i - 1));
 		}
-		//7. Print only the Strings that have the letter 'e' in them.
+		// 7. Print only the Strings that have the letter 'e' in them.
 		System.out.println();
+
 		for (int i = 0; i < bbt.size(); i++) {
 			if (bbt.get(i).contains("e")) {
 				System.out.println(bbt.get(i));
